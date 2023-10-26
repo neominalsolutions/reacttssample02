5d8a1bdb996a3e7bbf3caf08abf06e9421160ea5
neominalsolutions <neominal.solutions@gmail.com>
2023-10-26 11:04:38 +0300

    React Axios UseEffect Hook

---
+++ src/pages/PostsPage.tsx
@@ -0,0 +1,50 @@
+// postları yüklediğimiz component
+
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+// Backend deki Dto yapıları için burada karşılık olarak interface tercih edilir.
+// type mapping yaptığımızdan dolayı class ihtiyaç duymuyoruz.
+// DTO işlemleri için type yada interface tercih edebiliriz.
+export interface Post {
+  userId:number;
+  title:string;
+  body:string;
+  id:number;
+}
+
+function PostsPage() {
+
+  const [posts,setPosts] = useState<Post[]>([]);
+
+  return (
+    <>
+      <table style={{width:"100%", padding:"5px"}}>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+              posts.map((post:Post) => {
+                return  <tr key={post.id}>
+                  <td>
+                    <Link to={`posts/${post.id}`}>{post.title}</Link>
+                  </td>
+                 <td>
+                  {post.body}
+                 </td>
+                </tr>
+              })
+          }
+         
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default PostsPage
+
