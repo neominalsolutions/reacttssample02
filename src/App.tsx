b24b2f3472742b3c27053423d0596a570db0fc36
neominalsolutions <neominal.solutions@gmail.com>
2023-10-26 11:47:47 +0300

    PostsPage Component Tamamlandı

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Outlet, RouteObject, useRoutes } from 'react-router-dom';
+import { Link, Outlet, RouteObject, useRoutes } from 'react-router-dom';
 import PostsPage from './pages/PostsPage';
+import PostDetailPage from './pages/PostDetailPage';
 
 function App() {
   
   const routes:RouteObject[] = [
     {
       path:'',
-      element:<><Outlet /></>,
+      element:<> <Link to="/posts">Posts Page</Link> {' '} <Link to="/sample">Sample Page</Link>
+      <br></br><Outlet /></>,
       children: [
+        {
+          path:'/sample',
+          element: <>Sample</>
+        },
         {
           path:'/posts',
           Component:PostsPage
         },
         {
-          path:'/posts/:id' // dynamic route
+          path:'/posts/:id',
+          Component:PostDetailPage
+          // dynamic route
         }
       ]
     },
--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,5 +1,6 @@
 // postları yüklediğimiz component
 
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -7,7 +8,7 @@ import { Link } from 'react-router-dom';
 // type mapping yaptığımızdan dolayı class ihtiyaç duymuyoruz.
 // DTO işlemleri için type yada interface tercih edebiliriz.
 export interface Post {
-  userId:number;
+  userId?:number;
   title:string;
   body:string;
   id:number;
@@ -17,23 +18,44 @@ function PostsPage() {
 
   const [posts,setPosts] = useState<Post[]>([]);
 
-  useEffect(() => {
+  // birden falza case durumda bu load datayı çağırabiliriz.
+  const loadData = async()=> {
+    // response AxiosResponse döndürür.
+    let response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    let data = response.data; // response.body() denk gelir. fetch api da gördük
+    setPosts([...data]);
+  }
+
+   useEffect(() => { // ComponentDidMount denk gelir.
     console.log('component doma giriğinde');
+    // useEffect hook içerisinde async bir function kullanmak için aşağıdaki yazım formatını kullanalım.
+    loadData();
 
     // clean up function diyoruz. Component domdan ayrılırken çalışır.
-    return () => {
+    return () => { // Component WillUnMount
       console.log('component domdan çıktığında')
     }
-
   }, []) // [] hali empty dependecy herhangi bir state mekanizmasını takip etmediğimiz kısım. sadece doma girerken 1 kereye mahsus çalışır
 
   useEffect(() => {
-    console.log('posts state değiştiğinde');
-  }, [posts])
+    if(posts.length !== 0) {
+      console.log('posts state değiştiğinde');
+      // loadData();
+    }
+  }, [posts]) // [state] state takibi sağlar. // ComponentDidUpdate lifecyle method denk gelir. // ama birden falza useState olduğunda doma ilk giriş anında da tetiklenir.
 
+  // a veya b statelerinden herhangi birinin değişim anını yakalabiliriz.
+  // useEffect(()=> {
+
+  // }, [a,b])
+
+  const setStateSample = () => {
+    setPosts([{id:1,title:'t',body:'b'}])
+  }
 
   return (
     <>
+      <button onClick={setStateSample}>Test Change State</button>
       <table style={{width:"100%", padding:"5px"}}>
         <thead>
           <tr>
@@ -46,7 +68,7 @@ function PostsPage() {
               posts.map((post:Post) => {
                 return <tr key={post.id}>
                   <td>
-                    <Link to={`posts/${post.id}`}>{post.title}</Link>
+                    <Link to={`/posts/${post.id}`}>{post.title}</Link>
                   </td>
                  <td>
                   {post.body}
